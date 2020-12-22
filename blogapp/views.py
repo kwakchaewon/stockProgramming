902d79c2533a2e814bd0eebeabd29c6e91625067
kwak <ksh03003@naver.com>
2020-12-22 12:37:20 +0900

    day8.1

--- blogapp/views.py
+++ blogapp/views.py
@@ -46,6 +46,7 @@ def index(request):
 
 def index2(request):
     starttime=time.time()
+
     # 즐겨찾기한 목록 (예시 나중에는 오라클에서 가져올 예정)
     myStocks = ['aapl', 'qcom', 'tsla', 'amzn', 'amd', 'ba', 'dal', 'fb', 'googl', 'intc', 'ko', 'lulu', 'ma', 'msft',
                 'nflx', 'nke', 'nvda', 'sbux', 'tsm', 'uber', 'v']
@@ -91,45 +92,42 @@ def index3(request):
     myStocks = ['aapl', 'qcom', 'tsla', 'amzn', 'amd', 'ba', 'dal', 'fb', 'googl', 'intc', 'ko', 'lulu', 'ma', 'msft',
                 'nflx', 'nke', 'nvda', 'sbux', 'tsm', 'uber', 'v']
 
-    # 기존 비동기화 작업
-    # for i in range(0, len(myStocks)):
-    #     stockUrl= 'https://cloud.iexapis.com/stable/stock/' +myStocks[i] + '/book?token=pk_cc9d0be588704852a3e1b6e3c91b1e65'
-    #     async def asynconnect():
-    #         async with aiohttp.ClientSession () as session:
-    #             async with session.get(stockUrl) as response:
-    #                 aa=await response.text()
-    #                 print(aa)
-    #
-    #
-    #     loop = asyncio.new_event_loop()
-    #     asyncio.set_event_loop(loop)
-    #     loop.run_until_complete(asynconnect())
-    #     loop.close()
+    # apiList : html로 보내질 리스트 / value값들로 구성됨
+    apiList = []
 
     urls=[]
+
     #비동기로 웹페이지 가져오기
     for i in myStocks:
         urls.append('https://cloud.iexapis.com/stable/stock/' +
-                    i+ '/book?token=pk_cc9d0be588704852a3e1b6e3c91b1e65')
+                    i + '/book?token=pk_cc9d0be588704852a3e1b6e3c91b1e65')
 
     async def fetch(url):
+        # apiListValue: 각 종목의 주식 정보들이 포함될 딕셔너리
+        apiListValue = {}
+
         request2 = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
         response = await loop.run_in_executor(None, urlopen, request2)
         page = await loop.run_in_executor(None, response.read)
-        print(page)
+
+        stringPage=page.decode("utf-8")
+        jsonPage=json.loads(stringPage)
+
+        apiListValue["symbol"] = jsonPage["quote"]["symbol"]
+        apiListValue["cmpName"] = jsonPage["quote"]["companyName"]
+        apiListValue["latestPrice"] = str(jsonPage["quote"]["latestPrice"]) + "$"
+        apiList.append(apiListValue)
         return len(page)
 
     async def main():
         futures = [asyncio.ensure_future(fetch(url)) for url in urls]
-
         result = await asyncio.gather(*futures)
-        print(result)
 
 
-    loop=asyncio.new_event_loop()
+    loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(main())
     loop.close()
 
     print('로딩시간:'+str(time.time()-starttime))
-    return render(request, 'index3.html', {'test': sampleDate})
+    return render(request, 'index.html', {'stock_info': apiList})
