633144010565c79946b435d49642e9bb8f6a1b76
kwak <ksh03003@naver.com>
2021-01-24 00:48:04 +0900

    day 21.1

--- accounts/views.py
+++ accounts/views.py
@@ -29,8 +29,78 @@ def signup(request):
     return render(request, 'signup.html', {'todayIs': todayIs})
 
 
+
+
 def login(request):
     # 오늘 날짜 : ex) Novermber 30
     todayIs = str(datetime.today().strftime("%B %d"))
 
+
+    ## 로그인 4가지 경우의 수
+    ##  1_ userId 쿠키가 존재하며 정상적으로 로그인 되는 경우 => 메인화면으로
+    ##  2_ userId 쿠키는 존재하나 로그인은 되지 않는 경우(아이디나 비밀번호가 틀린 경우) => 로그인 화면으로
+    ##  3_ 로그인 쿠키가 존재하지 않아 요청받아 로그인이 된 경우 => 쿠키에 저장 후 메인 화면으로
+    ##  4_ 로그인 쿠키가 존재하지 않아 요청받아 로그인을 시도했으나 틀린 경우 => 로그인 창으로
+
+
+    # 쿠키 userId 값이 있다면 user로그인 시도
+    if request.COOKIES.get('userId') is not None:
+        userId = request.COOKIES.get('userId')
+        passwrd = request.COOKIES.get('passwrd')
+        user = auth.authenticate(request, username=userId, password=passwrd)
+
+
+        ##  1_ userId 쿠키가 존재하며 정상적으로 로그인 되는 경우 => 메인화면으로
+        if user is not None:
+            auth.login(request, user)
+            return redirect('http://127.0.0.1:8000/')
+
+
+        ##  2_ userId 쿠키는 존재하나 로그인은 되지 않는 경우(아이디나 비밀번호가 틀린 경우) => 로그인 화면으로
+        else:
+            return render(request, 'login.html', {'todayIs': todayIs, 'error': 'ID or password is incorrect'} )
+
+
+
+    #쿠키가 없는 경우
+    elif request.method == "POST":
+        userId = request.POST['userId']
+        passwrd = request.POST['userPwd']
+        user = auth.authenticate(request, username=userId, password=passwrd)
+
+
+        ##  3_ 로그인 쿠키가 존재하지 않아 요청받아 로그인이 된 경우 => 쿠키에 저장 후 메인 화면으로
+        if user is not None:
+            auth.login(request, user)
+
+
+            # if request.POST.get("keep_login") == "TRUE":
+            #     response = redirect('http://127.0.0.1:8000/')
+            #     response.set_cookie('userId', userId)
+            #     response.set_cookie('userPwd', passwrd)
+            #     return response
+
+            response = redirect('http://127.0.0.1:8000/')
+            response.set_cookie('userId', userId)
+            response.set_cookie('userPwd', passwrd)
+            return response
+
+
+            # return redirect('http://127.0.0.1:8000/')
+
+        ##  4_ 로그인 쿠키가 존재하지 않아 요청받아 로그인을 시도했으나 틀린 경우 => 로그인 창으로
+        else:
+            return (request, 'login.html', {'todayIs': todayIs , 'error': 'ID or password is incorrect'} )
+
+
+
+    ## 그밖에
+    else:
+        return render(request, 'login.html', {'todayIs': todayIs})
+
     return render(request, 'login.html', {'todayIs': todayIs})
+
+
+
+def logout(request):
+    respone = render(request, '')
